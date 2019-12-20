3de867754e8a40789f519e0fb0c7f4bc408cf140
filipdraganic <draganicfilip@gmail.com>
2019-12-20 01:19:57 +0100

    Dodato kliktanje na listIteme

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -22,7 +22,7 @@
                     <!-- END SIDEBAR USER TITLE -->
                     <!-- SIDEBAR BUTTONS -->
                     <div class="profile-userbuttons">
-                        <button type="button" class="btn btn-success btn-sm">Follow</button>
+                        <button type="button" class="btn btn-success btn-sm" v-on:click="promeniboju">Follow</button>
                     </div>
                     <!-- END SIDEBAR BUTTONS -->
                     <!-- SIDEBAR MENU -->
@@ -55,32 +55,109 @@
             </div>
             <div class="col-md-9">
                 <div class="profile-content">
-                    <mdb-list-group >
-                        <mdb-list-group-item href="#" :action="true" :active="this.boolean" id="prvi"  onclick="this.promeniboju" class="flex-column align-items-start">
-                            <div class="d-flex w-100 justify-content-between" >
-                                <h5 class="mb-1">List group item heading</h5>
-                                <small>3 days ago</small>
-                            </div>
-                            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                            <small>Donec id elit non mi porta.</small>
-                        </mdb-list-group-item>
-                        <mdb-list-group-item href="#" :action="true" :active="false" class="flex-column align-items-start">
+<!--                    <mdb-list-group>-->
+<!--                        <mdb-list-group-item href="#" data-alias="Nesto" :action="true" :active="true" id="prvi" v-on:click="console.log('AAAAAAAA')" class="flex-column align-items-start">-->
+<!--                            <div class="d-flex w-100 justify-content-between" >-->
+<!--                                <h5 class="mb-1">List group item heading</h5>-->
+<!--                                <small>3 days ago</small>-->
+<!--                            </div>-->
+<!--                            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--                            <small>Donec id elit non mi porta.</small>-->
+<!--                        </mdb-list-group-item>-->
+<!--                        <mdb-list-group-item href="#" :action="true" :active="false" class="flex-column align-items-start" v-on:click="console.log('AAAAAAAA')">-->
+<!--                            <div class="d-flex w-100 justify-content-between">-->
+<!--                                <h5 class="mb-1">List group item heading</h5>-->
+<!--                                <small class="text-muted">3 days ago</small>-->
+<!--                            </div>-->
+<!--                            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--                            <small class="text-muted">Donec id elit non mi porta.</small>-->
+<!--                        </mdb-list-group-item>-->
+<!--                        <mdb-list-group-item class="flex-column align-items-start" v-on:click="console.log('AAAAAAAA')">-->
+<!--                            <div class="d-flex w-100 justify-content-between">-->
+<!--                                <h5 class="mb-1">List group item heading</h5>-->
+<!--                                <small class="text-muted">3 days ago</small>-->
+<!--                            </div>-->
+<!--                            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>-->
+<!--                            <small class="text-muted">Donec id elit non mi porta.</small>-->
+<!--                        </mdb-list-group-item>-->
+<!--                    </mdb-list-group>-->
+
+                    <div class="list-group" id='listGroup'>
+                        <a  class="list-group-item flex-column align-items-start" v-bind:class="{ 'active' : boolprvi }" v-on:click="isSelected(boolprvi) ">
                             <div class="d-flex w-100 justify-content-between">
                                 <h5 class="mb-1">List group item heading</h5>
-                                <small class="text-muted">3 days ago</small>
                             </div>
                             <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                            <small class="text-muted">Donec id elit non mi porta.</small>
-                        </mdb-list-group-item>
-                        <mdb-list-group-item href="#" :action="true" class="flex-column align-items-start">
+                            <br>
+                        </a>
+                            <div class="containerZaRadioInput">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio111" value="option1">
+                                    <label class="form-check-label" for="inlineRadio111">1</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio222" value="option2">
+                                    <label class="form-check-label" for="inlineRadio222">2</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio333" value="option3">
+                                    <label class="form-check-label" for="inlineRadio333">3 </label>
+                                </div>
+                            </div>
+
+
+
+
+                        <a  class="list-group-item flex-column align-items-start" v-bind:class="{ 'active' : this.booldrugi  }" v-on:click="isSelected1(booldrugi)">
                             <div class="d-flex w-100 justify-content-between">
                                 <h5 class="mb-1">List group item heading</h5>
-                                <small class="text-muted">3 days ago</small>
                             </div>
                             <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                            <small class="text-muted">Donec id elit non mi porta.</small>
-                        </mdb-list-group-item>
-                    </mdb-list-group>
+                            <br>
+                        </a>
+                        <div class="containerZaRadioInput">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio11" value="option1">
+                                <label class="form-check-label" for="inlineRadio11">1</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio22" value="option2">
+                                <label class="form-check-label" for="inlineRadio22">2</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio33" value="option3">
+                                <label class="form-check-label" for="inlineRadio33">3 </label>
+                            </div>
+                        </div>
+
+
+
+                        <a  class="list-group-item flex-column align-items-start" v-bind:class="{ 'active' : this.booltreci  }" v-on:click="isSelected2(booltreci)">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">List group item heading</h5>
+                            </div>
+                            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                            <br>
+                        </a>
+                        <div class="containerZaRadioInput">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio1" value="option1">
+                                <label class="form-check-label" for="inlineRadio1">1</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio2" value="option2">
+                                <label class="form-check-label" for="inlineRadio2">2</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions3" id="inlineRadio3" value="option3">
+                                <label class="form-check-label" for="inlineRadio3">3 </label>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
             </div>
         </div>
@@ -91,6 +168,7 @@
     import { mdbListGroup, mdbListGroupItem } from 'mdbvue';
     import $ from 'jquery'
     import 'jquery'
+
     export default {
 
         name: "UserPage",
@@ -99,7 +177,10 @@
         },
         data(){
             return{
-                boolean:'true'
+                boolean:'true',
+                boolprvi:false,
+                booldrugi:false,
+                booltreci:false
             }
 
         },
@@ -108,26 +189,32 @@
             mdbListGroupItem
         },
         methods:{
+            isSelected: function(i){
+                //send http request to toggle subscription on service
+                return this.boolprvi  = !i;
+
+            },
+            isSelected1: function(i){
+                return this.booldrugi  = !i;
+
+            },isSelected2: function(i){
+                return this.booltreci  = !i;
+
+            },
+
             promeniboju : function(){
-                console.log("promeniBoju")
+                console.log("promeniBoju" + this.selected)
                 // document.getElementsByTagName("prvi").setAttribute(":active", "true");
 
             }
         },
         mounted(){
-            $(document).ready(function() {
-                $('.mdb-list-group-item').on('click', function() {
-                    var $this = $(this);
-                    var $alias = $this.data('alias');
+            $(document).on('click', '#eventList .mdb-list-group-item', function(e) {
+                this.promeniboju()
+                console.log("nesto")
 
-                    $('.active').removeClass('active');
-                    $this.toggleClass('active')
-                    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
-                    // Pass clicked link element to another function
-                    // myfunction($this, $alias)
-                })
             });
-        }
+            }
     }
 </script>
 
@@ -135,6 +222,12 @@
 
     body {
         background: #F1F3FA;
+    }
+
+
+    .containerZaRadioInput{
+        margin-top: -25px;
+        z-index: 3;
     }
 
     /* Profile container */
